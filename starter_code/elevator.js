cd28acb254be0954ce5d34c2f52f5e7b298369bb
MARTIN GENET <martingenet@MHG.local>
2018-02-12 17:54:00 -0500

    i think i did ok

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,82 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = "";
+    this.intervalId;
+    this.waitingList = [];
+    this.passenger = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() { 
+  this.intervalId = setInterval(() =>{
+    this.update();
+  } 1000);
+  }
+
+  stop() { 
+    clearInterval(this.intervalId);
+  }
+  update() {
+    this.log();
+ 
+
+ 
+    
+    if (this.floor > this.requests[0]){
+      this.floorUp();
+    }else{
+      this.floorDown();
+    }
+   }
+  _passengersEnter(person) {
+    if (this.waitingList.includes(this.floor)){
+      this.passenger.push(person);
+      for(var i = this.waitingList.length - 1; i >= 0; i--) {
+    if(this.waitingList[i] === this.floor) {
+       this.waitingList.splice(i, 1);
+    }
+    if (this.requests[i] === this.floor) {
+      this.requests.splice(i,1);
+    }
+}
+}
+   }
+  _passengersLeave() { 
+    if (this.passenger.includes(this.floor)) {
+      for(var i = this.passenger.length - 1; i >= 0; i--) {
+        if(this.passenger[i] === this.floor) {
+           this.passenger.splice(i, 1);
+        }
+    }
+  }
+
+
+
+  floorUp() {
+    if (this.floor < this.MAXFLOOR){
+      console.log("hi")
+      this.direction = 'up';
+      this.floor++;
+    }
+   }
+  floorDown() {
+    if (this.floor > 0){
+      this.direction = 'down';
+      this.floor--;
+    }
+   }
+
+  //  In the Elevator class, it's time to write the code for the
+//  call method. This method should receive a person object and add it 
+//  as a request into the elevator’s queue.
+  call(person) {
+    this.waitingList.push(this.person)
+    this.requests.push(person.originFloor);
+   }
+  log() {
+    console.log(this.direction);
+    console.log(`Direction: ${this.direction} | Floor:${this.floor}`);
+   }
 }
 
 module.exports = Elevator;
--- starter_code/person.js
+++ starter_code/person.js
@@ -1,6 +1,30 @@
+
+// A person should have three attributes:
+
+// A name
+// A originFloor: the floor they are in when they call the elevator
+// A destinationFloor: the floor they intend to go to
+
 class Person {
   constructor(name, originFloor, destinationFloor){
+    this.name = name;
+    this.originFloor = originFloor;
+    this.destinationFloor = destinationFloor;
   }
 }
 
 module.exports = Person;
+
+
+
+// Now, we need to think about what happens when a person ‘calls’ the 
+// elevator. In the Elevator class, it's time to write the code for the
+//  call method. This method should receive a person object and add it 
+//  as a request into the elevator’s queue.
+
+// Add the whole person object to the requests array. The elevator 
+// will need all that information later.
+
+// In later iterations, the elevator will process the list of requests 
+//  it travels up and down. We will need a list of floors that the 
+//  elevator should serve in the update method.
